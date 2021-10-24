62c79942d6bd04ba5e50e4aba3ac14fd4e52bdf7
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-10-24 14:07:02 +0200

    populate price and number of selected seats

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -1,5 +1,5 @@
 const moviePick = document.querySelector('#movie');
-const showCase = document.querySelector('.showcase');
+// const showCase = document.querySelector('.showcase');
 const seatsContainer = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat');
 const count = document.getElementById('count');
@@ -8,12 +8,26 @@ const total = document.getElementById('total');
 //price of movie
 let moviePrice = moviePick.value;
 
+function updatePrice(selectedSeats) {
+    moviePick.addEventListener('change', () => {
+        moviePrice = moviePick.value;
+        total.innerText = selectedSeats.length * moviePrice;
+    })    
+}
+
 
 function markSeats(e) {
     if(e.target.matches('.seat') && !e.target.matches('.occupied')){
-        e.target.classList.toggle('selected')
-        countPrice();
+        e.target.classList.toggle('selected');
     }
+    const selectedSeats = seatsContainer.querySelectorAll('.selected');
+
+    count.innerText = selectedSeats.length;
+    total.innerText = selectedSeats.length * moviePrice;
+
+    updatePrice(selectedSeats);
+    
+
 }
 
 
