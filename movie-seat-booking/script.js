f725b6ffbb21ae85b04e3fa34019f2eb07fa2827
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-10-24 18:48:33 +0200

    working on localStorage

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -8,11 +8,43 @@ const total = document.getElementById('total');
 //price of movie
 let moviePrice = moviePick.value;
 
-function updatePrice(selectedSeats) {
+function restoreData(){
+    const restoredSeats = JSON.parse(localStorage.getItem('selectedSeatsIndex'));
+    const restoredMovie = localStorage.getItem('pickedMovie');
+
+    //populate seats
+    seats.forEach( (seat, index) => {
+        if (restoredSeats.includes(index)){
+            seat.classList.add('selected');
+        };
+    })
+}
+
+function storageData(selectedSeats){
+    //Saving index of seats
+    const selectedSeatsIndex = [...selectedSeats].map( seat => 
+        [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeatsIndex', JSON.stringify(selectedSeatsIndex));
+    // localStorage.setItem('countSeats', count.innerText);        
+    // localStorage.setItem('price', total.innerText);
+    localStorage.setItem('pickedMovie', moviePick.value)
+}
+
+function updateData() {
+    
+    const selectedSeats = seatsContainer.querySelectorAll('.selected');
+
     moviePick.addEventListener('change', () => {
-        moviePrice = moviePick.value;
-        total.innerText = selectedSeats.length * moviePrice;
-    })    
+        moviePrice = moviePick.value; 
+        updatePrice();
+    })
+
+    count.innerText = selectedSeats.length;
+    total.innerText = selectedSeats.length * moviePrice;
+    
+    storageData(selectedSeats);
+            
 }
 
 
@@ -20,20 +52,13 @@ function markSeats(e) {
     if(e.target.matches('.seat') && !e.target.matches('.occupied')){
         e.target.classList.toggle('selected');
     }
-    const selectedSeats = seatsContainer.querySelectorAll('.selected');
-
-    count.innerText = selectedSeats.length;
-    total.innerText = selectedSeats.length * moviePrice;
-
-    updatePrice(selectedSeats);
-    
-
+    updateData();
 }
 
 
 seatsContainer.addEventListener('click', (e) => markSeats(e))
 
-
+restoreData();
 
 // document.addEventListener('click', (e) => {
 //     console.log(e.target);
