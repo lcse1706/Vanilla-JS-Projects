06bc7ccab1c34e6576cf9a223710ba2659ec4da0
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-11-04 20:20:40 +0100

    handle wrong letter

--- hangman-game/script.js
+++ hangman-game/script.js
@@ -1,12 +1,18 @@
 const word = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
 
-const wordContainer = ['programming', 'frontEnd'];
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['programming', 'frontEnd'];
 
 let correctLetters = [];
-let wrogLetters = [];
+let wrongLetters = [];
 
-const selectedWord =
-  wordContainer[Math.floor(Math.random() * wordContainer.length)];
+const selectedWord = words[Math.floor(Math.random() * wordContainer.length)];
 
 function displayWord() {
   word.innerHTML = ` ${selectedWord
@@ -20,6 +26,35 @@ function displayWord() {
     )
     .join('')}
     `;
+
+  const innerWord = word.innerText.replace(/\n/g, '');
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations! You won! 😃';
+    popup.style.display = 'flex';
+  }
+}
+
+function WrongLettersEl() {
+  wrongLettersEl.innerHTML = `
+  ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+  ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.dispaly = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost. 😕';
+    popup.style.display = 'flex';
+  }
 }
 
 displayWord();
