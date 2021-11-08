d01ed8070807ceac7c35797244d82f6703388b33
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-11-08 21:35:18 +0100

    display recipie under search result

--- meal-finder/script.js
+++ meal-finder/script.js
@@ -2,6 +2,7 @@ const input = document.getElementById('search');
 const resultHeading = document.getElementById('result-heading');
 const meals = document.getElementById('meals');
 const submit = document.getElementById('submit');
+const single_mealEl = document.getElementById('single-meal');
 
 function searchMeal(e) {
   e.preventDefault();
@@ -17,7 +18,6 @@ function searchMeal(e) {
   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
       if (data.meals === null) {
         resultHeading.innerHTML = `<p>There are no search results for ${term}</p>`;
@@ -40,4 +40,55 @@ function searchMeal(e) {
   search.value = '';
 }
 
+function showRecipie(e) {
+  const item = e.target;
+
+  if (item.classList.contains('meal-info')) {
+    const mealId = item.getAttribute('data-mealID');
+    getMealByID(mealId);
+  }
+}
+
+function getMealByID(mealId) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      addMealtoDOM(data);
+    });
+}
+
+function addMealtoDOM(item) {
+  const ingredients = [];
+  const mealEl = item.meals[0];
+
+  for (let i = 1; i <= 20; i++) {
+    if (mealEl[`strIngredient${i}`]) {
+      ingredients.push(
+        `${mealEl[`strIngredient${i}`]} - ${mealEl[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+  single_mealEl.innerHTML = `
+  <div class="single-meal">
+    <h1>${mealEl.strMeal}</h1>
+    <img src="${mealEl.strMealThumb}" alt="${mealEl.strMeal}" />
+    <div class="single-meal-info">
+      ${mealEl.strCategory ? `<p>${mealEl.strCategory}</p>` : ''}
+      ${mealEl.strArea ? `<p>${mealEl.strArea}</p>` : ''}
+    </div>
+    <div class="main">
+      <p>${mealEl.strInstructions}</p>
+      <h2>Ingredients</h2>
+      <ul>
+        ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+      </ul>
+    </div>
+  </div>
+`;
+}
+
 submit.addEventListener('submit', searchMeal);
+
+meals.addEventListener('click', showRecipie);
