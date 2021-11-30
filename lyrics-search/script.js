3b7dbaeb49c44e16ee856076ce362109546b98e4
Lukasz Czarniecki <lcse.2001@gmail.com>
2021-11-30 12:02:55 +0100

    fetch data, add to DOM, next prev buttons

---
+++ lyrics-search/script.js
@@ -0,0 +1,63 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+async function searchSong(term) {
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+function showData(data) {
+  result.innerHTML = `
+    <ul class="songs">
+        ${data.data
+          .map(
+            (song) => `<li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>`
+          )
+          .join('')}
+    </ul>`;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ''
+      } 
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ''
+      }    
+ `;
+  } else {
+    more.innerHTML = '';
+  }
+}
+
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+
+  if (!searchTerm) {
+    alert('Please type in search term');
+  } else {
+    searchSong(searchTerm);
+  }
+});
